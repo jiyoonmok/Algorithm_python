d4ea8077ceff8d7daa6af7a36650e94070e29640
Jiyoon Mok <jymok007@gmail.com>
2022-02-17 03:21:25 +0900

    22.02.16

--- code_practice/소수(에라토스테네스 체).py
+++ code_practice/소수(에라토스테네스 체).py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import sys
-#sys.stdin=open("input.txt","rt")
-N = int(input())
-list=[]
-
-for n in range(2,N+1):
-    for i in range(2,n):
-        if n%i == 0:
-            break
-    else:
-         list.append(n)
-
-print(len(list))
+sys.stdin=open("input.txt", "r")
+n=int(input())
+ch=[0]*(n+1)
+cnt=0
+for i in range(2, n+1):
+    if ch[i]==0:
+        cnt+=1  # 값이 0인것만 카운팅
+        for j in range(i, n+1, i):
+            ch[j]=1   # 소수가 아닌 것은 1 넣기
+print(cnt)
